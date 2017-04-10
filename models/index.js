33b30e726eb1019ce680a51d240b49325656f910
shockeyt <shockeyt@gmail.com>
2017-04-10 15:38:29 -0600

    setup heroku

--- models/index.js
+++ models/index.js
@@ -4,4 +4,6 @@ mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/personal-api");
 
+// mongoose.connect( process.env.MONGODB_URI || "https://whispering-garden-62967.herokuapp.com/" );
+
 // module.exports.Campsite = require("./campsite.js.example");
