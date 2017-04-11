a329eb9429e9ac372520c03c7a70674e67e61c47
shockeyt <shockeyt@gmail.com>
2017-04-10 18:15:42 -0600

    added exec function to index route

--- server.js
+++ server.js
@@ -83,10 +83,15 @@ app.get('/api/profile', function profile(req, res) {
 
 //show all synths
 app.get('/api/synths', function (req, res) {
-  db.Synth.find({}, function(err, synths) {
-    if (err) {
-      console.log("Error: ", err);
-    }
+  // db.Synth.find({}, function(err, synths) {
+  //   if (err) {
+  //     console.log("Error: ", err);
+  //   }
+  //   res.json(synths);
+  // });
+  db.Synth.find()
+  .exec(function(err, synths) {
+    if (err) { return console.log("index error: " + err); }
     res.json(synths);
   });
 
